4988398e65303b5d526bf1e589585abdd204c184
Mickael <mickael@lempire.co>
2025-12-11 15:45:45 +0100

    Add floor click event handler and documentation
    - Add mouseup event handler for floor element
    - Add id="floor" to floor transform in main.html
    - Add important notes explaining Meteor packages (insecure, autopublish)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { Template } from 'meteor/templating';
 
+/*
+Important notes
+
+1) this Meteor project includes 2 special packages
+- insecure: allows the client to write data (meaning that you don't need Meteor methods to write data to the database)
+- autopublish: allows the client to access all server data without need of a publication/subscription
+
+2) you only need to look at main.js and main.html files to understand the code.
+*/
+
 if (Meteor.isClient) {
 
   Template.world.helpers({
@@ -7,11 +17,14 @@ if (Meteor.isClient) {
   });
 
   Template.world.events({
+    'mouseup #floor'(event) {
+      console.log('mouseup on floor', event);
+    }
 
   });
 
 }
 
 if (Meteor.isServer) {
-  
+  // Server code
 }
